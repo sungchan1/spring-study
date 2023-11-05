32a47c145083bc0f27018377bbba217c5f129322
sungchan1 <sungchan254@naver.com>
2023-11-05 22:53:22 +0900

    DI 주입 & H2 DB

--- hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -4,6 +4,8 @@ import hello.hellospring.domain.Member;
 
 import java.util.*;
 
+
+
 public class MemoryMemberRepository implements MemberRepository{
     private static  Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
