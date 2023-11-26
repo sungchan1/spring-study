d80959e8bbf7f0fa2ab3cd9ca77d8c74d3c9cca8
sungchan Yeo <sungchan254@naver.com>
2023-11-26 16:45:23 +0900

    Spring basic day 10
    Fininsh

---
+++ spring-basic/src/test/java/hello/springbasic/network/NetworkClient.java
@@ -0,0 +1,51 @@
+package hello.springbasic.network;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class NetworkClient
+{
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    // 서비스 시작시 호출
+    public void connect(){
+        System.out.println("connect = " + url);
+    }
+
+    public void call(String messages) {
+        System.out.println("call: " + url + " messgae = " + messages);
+
+    }
+
+
+    // 서비스 종료시 호출
+    public void disconnect() {
+        System.out.println("close " + url);
+    }
+
+
+    @PostConstruct
+    public void init()  {
+        System.out.println("NetwrokClient.init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+
+    @PreDestroy
+    public void close() {
+        System.out.println("NetwrokClient.close");
+        disconnect();
+    }
+}
