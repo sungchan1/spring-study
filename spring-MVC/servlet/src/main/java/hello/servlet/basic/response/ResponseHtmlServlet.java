1978f23a8d53719c569d841173ce457d62c3a718
sungchan Yeo <sungchan254@naver.com>
2024-01-08 23:08:13 +0900

    Spring MVC day 5
    finish Servlet

---
+++ spring-MVC/servlet/src/main/java/hello/servlet/basic/response/ResponseHtmlServlet.java
@@ -0,0 +1,9 @@
+package hello.servlet.basic.response;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(name = "responseHtmlServlet", urlPatterns = "response-html")
+public class ResponseHtmlServlet extends HttpServlet {
+
+}
