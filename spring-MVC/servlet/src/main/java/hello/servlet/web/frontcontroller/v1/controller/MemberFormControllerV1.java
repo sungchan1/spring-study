1ae3af6616dd9373227ac4e9573a6fdfef4cc678
sungchan Yeo <sungchan254@naver.com>
2024-01-10 23:52:47 +0900

    Spring MVC day 7 -2
    create MVC Framework

---
+++ spring-MVC/servlet/src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberFormControllerV1.java
@@ -0,0 +1,19 @@
+package hello.servlet.web.frontcontroller.v1.controller;
+
+import hello.servlet.web.frontcontroller.v1.ControllerV1;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+ 
+public class MemberFormControllerV1 implements ControllerV1 {
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String viewPath = "/WEB-INF/views/new-form.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath); // 컨트롤러 -> 뷰로 이동할 때
+        dispatcher.forward(request, response); // 서블릿에서 JSP를 호출, 서버 내부에서 다시 호출한다.
+    }
+
+}
